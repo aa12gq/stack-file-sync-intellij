700d2080b92a3996e730fd54cd072ed101dac37e
aa12gq <aa12gq@gmail.com>
2025-12-15 14:06:41 +0800

    chore: bump stack-sync formula to v1.2.0

--- homebrew-stack-sync/Formula/stack-sync.rb
+++ homebrew-stack-sync/Formula/stack-sync.rb
@@ -1,14 +1,14 @@
 class StackSync < Formula
   desc "Interactive file synchronization tool for development teams"
   homepage "https://github.com/aa12gq/stack-file-sync-intellij"
-  version "1.1.5"
+  version "1.2.0"
   license "MIT"
   
   head "https://github.com/aa12gq/stack-file-sync-intellij.git", branch: "main"
 
   # Use universal binary for macOS (supports both Intel and ARM)
-  url "https://github.com/aa12gq/stack-file-sync-intellij/releases/download/v1.1.5/stack-sync-darwin-universal.tar.gz"
-  sha256 "1c8f9a7063fd4952f2471deb0f740d596fec7039fd7e9a7ca83a2d72cc9094ce"
+  url "https://github.com/aa12gq/stack-file-sync-intellij/releases/download/v1.2.0/stack-sync-darwin-universal.tar.gz"
+  sha256 "f2b2cbce9d1c48b0526ca9b7ebe84d5eff7cb090ea928205616a789257a09e65"
 
   def install
     bin.install "stack-sync"
